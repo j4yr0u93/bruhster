5a0d8a957be3ddeeb73b0a3e08910351e9e090c5
j4yr0u93 <lourie10@gmail.com>
2020-09-04 19:19:36 -0400

    Update node_worker.py

--- bruhster_node/node_worker.py
+++ bruhster_node/node_worker.py
@@ -1,1 +1,38 @@
+#vanilla liberals
 import datetime
+import time
+import sys
+import subprocess
+
+#kinky liberals
+import asyncio
+import uvloop
+import pynat
+import pynacl
+import aiohttp
+import pillow
+
+#audiophile liberals
+import spotipy
+
+#content liberals
+import twitch
+
+#gaymer liberals
+import discord
+from discord.utils import oauth_url
+
+#im fast as fug boi
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+#main
+def main():
+
+#user joins sync and get added into queue
+@client.event
+async def on_join(viewer):
+
+#queue mod update
+@client.event
+async def on_queue_change(viewer.queue):
