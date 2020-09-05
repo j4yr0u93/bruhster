f70b04d09fce29be112bc66f0758a4e810f18643
j4yr0u93 <lourie10@gmail.com>
2020-09-04 21:08:48 -0400

    rethonk

---
+++ bruhster/client.py
@@ -0,0 +1,49 @@
+#vanilla liberals
+import datetime
+import time
+import sys
+import subprocess
+
+#kinky liberals
+import asyncio
+import pynat
+import pynacl
+import aiohttp
+import pillow
+import sanic
+
+#audiophile liberals
+import spotipy
+
+#content liberals
+import twitch
+
+#gaymer liberals
+import discord
+from discord.utils import oauth_url
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic("App Name")
+
+@app.route("/")
+async def test(request):
+    return json({"hello": "world"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+#im fast as fug boi
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+#main
+def main():
+
+#user joins sync and seeks to current position in track and gains queue
+class streamer:
+    def __init(streamer_token):
+
+class viewer:
+    def __init__(viewer_token):
