2820db1a08debf044b489e469852bf7117ec4fc8
j4yr0u93 <lourie10@gmail.com>
2020-10-29 11:27:50 -0400

    session functionality

--- bruhster_server/aiohttp_bruhster/server.py
+++ bruhster_server/aiohttp_bruhster/server.py
@@ -5,15 +5,34 @@ import gino
 import datetime
 import time
 import sys
+import os
 import subprocess
+import base64
+import spotipy
+import uuid
+from cryptography import fernet
 from aiohttp import web
+from aiohttp_session import setup, get_session, session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from aiohttp_bruhster.routes import setup_routes
 from aiohttp_bruhster import web_config
-#from aiohttp_session import setup, get_session, session_middleware
-#from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 
-#start web server
-app = web.Application()
-setup_routes(app)
-web.run_app(app, host=web_config['WEB_HOST'], port=web_config['WEB_PORT'])
+#start web server with sessions(aes cookies)
+async def handler(request):
+    session = await get_session(request)
+    last_visit = session['last_visit'] if 'last_visit' in session else None
+    session['last_visit'] = time.time()
+    text = 'Last visited: {}'.format(last_visit)
+    return web.Response(text=text)
+
+def make_app():
+    app = web.Application()
+    # secret_key must be 32 url-safe base64-encoded bytes
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key))
+    setup_routes(app)
+    return app
+
+web.run_app(make_app(), host=web_config['WEB_HOST'], port=web_config['WEB_PORT'])
